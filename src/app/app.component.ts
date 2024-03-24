939b1a3f13b2cbc142fff7a56a59e5437c76c8df
refatul-fahad <refatm452@gmail.com>
2024-03-25 01:14:55 +0600

    Added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,133 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChartModule } from 'primeng/chart';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChartModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ClaimsForecasts';
+  basicData: any;
+  data: any;
+  basicOptions: any;
+  options: any;
+
+  ngOnInit() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.basicData = {
+          labels: ['Q1', 'Q2', 'Q3', 'Q4','Q3', 'Q4'],
+          datasets: [
+              {
+                  label: 'Claim Amount',
+                  data: [540, 325, 702, 620,702, 620],
+                  backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)'],
+                  borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)'],
+                  borderWidth: 1
+              }
+          ]
+      };
+
+      this.basicOptions = {
+          plugins: {
+              legend: {
+                  labels: {
+                      color: textColor
+                  }
+              }
+          },
+          scales: {
+              y: {
+                  beginAtZero: true,
+                  ticks: {
+                      color: textColorSecondary
+                  },
+                  grid: {
+                      color: surfaceBorder,
+                      drawBorder: false
+                  },
+                  title:{
+                    display:true,
+                    text: 'Claim Amount',
+                    font: {
+                      weight: 'bold',
+                      size: 14,
+                      family: 'Arial'
+                    }
+                  }
+              },
+              x: {
+                  ticks: {
+                      color: textColorSecondary
+                  },
+                  grid: {
+                      color: surfaceBorder,
+                      drawBorder: false
+                  },
+                  title:{
+                    display:true,
+                    text: 'Month',
+                    font: {
+                      weight: 'bold',
+                      size: 16,
+                      family: 'Arial'
+                    }
+                  }
+              }
+          }
+      };
+
+      this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Claim Amount',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: documentStyle.getPropertyValue('--blue-500'),
+                tension: 0.4
+            }
+        ]
+    };
+
+    this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            }
+        }
+    };
+  }
 }
